dad391aabe22790419b25763c87c1a8bad4877e4
cyborpaul <leowaldir2000@gmail.com>
2022-01-11 19:32:53 -0500

    Rutas

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define("BASE_URL", "http://localhost/monitoreo/");
+define("DEFAULT_PROYECT_PATH","/monitoreo/");
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-    phpinfo();
+
+/* Obtener URL */
+$requestUrl = parse_url($_SERVER['REQUEST_URI']);
+$urlPath = $requestUrl["path"];
+$url =explode("/", $urlPath);
+
+/* Formatear la url que ya es un arreglo */
+$lastIndexUrl=count($url)-1;
+if(empty($url[$lastIndexUrl])){
+    array_shift($url);
+    array_pop($url);
+}else{
+    array_shift($url);
+}
+/* Fin de formato de la Url */
+require_once "config/config.php";
+
+/* Incluir archivs de configuración */
+
+
+/* Comprobación */
+if(DEFAULT_PROYECT_PATH===$urlPath){
+    echo "Rutas son iguales cargar controlador predeterminado";
+}elseif(DEFAULT_PROYECT_PATH===($urlPath . "/")){
+    echo "Rutas no son iguales cargar controlador predeterminado";
+}else{
+    echo "Otro controlador";
+}
+/* Fin comprobación de Url */
 ?>
+
+
+<link rel="stylesheet" href="<?php echo BASE_URL?>style.css">
