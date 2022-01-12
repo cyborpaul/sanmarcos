8632bddf17008ba9b7ed0474fa6dd5c5b8260374
cyborpaul <leowaldir2000@gmail.com>
2022-01-11 19:42:53 -0500

    Rutas2

--- config/config.php
+++ config/config.php
@@ -1,5 +1,8 @@
 <?php
-define("BASE_URL", "http://localhost/monitoreo/");
-define("DEFAULT_PROYECT_PATH","/monitoreo/");
+/* define("BASE_URL", "http://localhost/monitoreo/");
+define("DEFAULT_PROYECT_PATH","/monitoreo/"); */
+
+define("BASE_URL", "http://inventalo.pe/sanmarcos/");
+define("DEFAULT_PROYECT_PATH","/sanmarcos/");
 
 ?>
