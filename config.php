85ae825f491ebc8a8633d92ab213d1cda0f75963
cyborpaul <leowaldir2000@gmail.com>
2022-03-12 19:01:17 -0500

    WEBSERVICE

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 		
-		$mysqli = new mysqli('localhost', 'root', 'RPJsh8E4a9#C9v', 'sanmarcos');
+		$mysqli = new mysqli('localhost', 'root', '', 'sanmarcos');
 		if ($mysqli->connect_errno) {
 			echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 		}
--- in_datos.php
+++ in_datos.php
@@ -4,6 +4,8 @@ $dato= $_GET['dato'];
 require("config.php");
 mysqli_query($mysqli,"INSERT INTO datos (dato) VALUES('$dato')");
 
+json_encode("INGRESADO");
+
 
 
 ?>
